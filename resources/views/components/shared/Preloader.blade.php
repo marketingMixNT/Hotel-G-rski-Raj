aef29911b199d561edb72ae6a54c841db5462660
Marek <marekgacek45@gmail.com>
2024-06-26 09:11:56 +0200

    update preloader

--- resources/views/components/shared/Preloader.blade.php
+++ resources/views/components/shared/Preloader.blade.php
@@ -1,7 +1,10 @@
-<div id="preloader" class="flex flex-col justify-center items-center fixed top-0 right-0 left-0 bottom-0 opacity-100 transition-opacity duration-1000 ease-in-out bg-secondary-200 " style="z-index: 1000000">
-    <img src="{{asset('assets/logo/logo--white.png')}}" alt="logo Zajazdu Śleboda" width="200px" class="w-[150px]  md:w-[200px]">
-    <span class="font-heading text-2xl text-white mt-3">Hotel Górski Raj</span>
-      
-    
-    </div>
+<div id="preloader"
+    class="fixed inset-0 flex flex-col justify-center items-center gap-y-1 bg-primary-600 transition-opacity duration-1000 ease-in-out"
+    style="z-index: 1000000">
+
+    <img src="{{ asset('assets/logo/logo.png') }}" alt="logo Hotelu Górski Raj" width="96" height="50"
+        class="w-[150px]  md:w-[200px]" />
+    <span class="font-heading text-2xl  md:text-4xl ">Hotel Górski Raj</span>
+
 </div>
+
